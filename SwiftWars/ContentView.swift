d32e6423ea521e730ae859236fac38d61be60f50
Vladislav Prusakov <spectraldragonchannel@gmail.com>
2020-02-21 13:57:45 +0300

    Initial Commit

---
+++ SwiftWars/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftWars
+//
+//  Created by Vladislav Prusakov on 21.02.2020.
+//  Copyright © 2020 AO Tinkoff. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
